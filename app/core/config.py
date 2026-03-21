84d5db55aa4fde81126f1060aaa7da646c475ec0
fender <parser139@gmail.com>
2026-03-21 20:08:23 +0600

    1

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
-
+    COOKIE_SECURE: bool = False
+    COOKIE_SAMESITE: str = "lax"  # lax | strict | none
+    COOKIE_DOMAIN: str | None = None
+    COOKIE_MAX_AGE: int = 60 * 60 * 24
     model_config = SettingsConfigDict(
         env_file=".env",
     )
